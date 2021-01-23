f7db6aff4e14f75c4571111dcb13007a6d466259
Clay <ctpence@google.com>
2021-01-22 17:12:26 -0800

    Fix test cluster names (#365)

--- scripts/asm-installer/tests/common.sh
+++ scripts/asm-installer/tests/common.sh
@@ -237,7 +237,7 @@ create_working_cluster() {
 
   gcloud beta container \
     --project "${PROJECT_ID}" \
-    clusters create "ephem-${CLUSTER_NAME}" \
+    clusters create "${CLUSTER_NAME}" \
     --zone "${CLUSTER_LOCATION}" \
     --no-enable-basic-auth \
     --release-channel "regular" \
