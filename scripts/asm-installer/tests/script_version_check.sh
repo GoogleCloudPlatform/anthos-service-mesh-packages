697a8509b2935ae6680bbe5a2d22259f2ce4b86c
Jason Wang <jasonwzm@google.com>
2021-02-26 13:34:36 -0800

    Add a version check of scripts (#506)
    * Add a version check of scripts
    
    * newline
    
    * fix test
    
    * address comments
    
    * typo

---
+++ scripts/asm-installer/tests/script_version_check.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+set -eu
+
+INSTALL_ASM_SCRIPT="install_asm"; readonly INSTALL_ASM_SCRIPT;
+ASM_VM_SCRIPT="asm_vm"; readonly ASM_VM_SCRIPT;
+
+while read -r KEYWORD; do
+  INSTALL_ASM_LINE="$(grep "^${KEYWORD}=" "${INSTALL_ASM_SCRIPT}")"
+  if [[ -z "${INSTALL_ASM_LINE}" ]]; then
+    echo "Cannot find line starting with ${KEYWORD}= in install_asm"
+    exit 1
+  fi
+  ASM_VM_LINE="$(grep "^${KEYWORD}=" "${ASM_VM_SCRIPT}")"
+  if [[ -z "${ASM_VM_LINE}" ]]; then
+    echo "Cannot find line starting with ${KEYWORD}= in asm_vm"
+    exit 1
+  fi
+  if [[ "${INSTALL_ASM_LINE}" != "${ASM_VM_LINE}" ]]; then
+    echo "${KEYWORD} version line does not match in install_asm and asm_vm. Please make the change."
+    exit 1
+  fi
+done <<EOF
+MAJOR
+MINOR
+POINT
+REV
+EOF
+
+echo "Success: versions in the scripts are verified."
