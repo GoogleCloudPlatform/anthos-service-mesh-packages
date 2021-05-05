36d86ed6bd1fd0123349f4fad4e0c290cabcd7d1
Zhengzhe Yang <zhengzhey@google.com>
2021-05-05 19:16:23 +0000

    addressing comments

--- asmcli/asmcli.sh
+++ asmcli/asmcli.sh
@@ -72,62 +72,10 @@ KPT_BRANCH=""
 NAMESPACE_EXISTS=0
 KUBECONFIG_SUPPLIED=0
 
-### Context init JSON ###
-CONTEXT_JSON=$(cat <<EOF
-{
-  "flags": {
-    "PROJECT_ID": "${PROJECT_ID:-}",
-    "CLUSTER_NAME": "${CLUSTER_NAME:-}",
-    "CLUSTER_LOCATION": "${CLUSTER_LOCATION:-}",
-    "MODE": "${MODE:-}",
-    "CA": "${CA:-}",
-    "KUBECONFIG": "${KUBECONFIG_FILE:-}",
-    "CONTEXT": "${CONTEXT:-}",
-    "CUSTOM_OVERLAY": "${CUSTOM_OVERLAY:-}",
-    "OPTIONAL_OVERLAY": "${OPTIONAL_OVERLAY:-}",
-    "ENABLE_ALL": ${ENABLE_ALL:-0},
-    "ENABLE_CLUSTER_ROLES": ${ENABLE_CLUSTER_ROLES:-0},
-    "ENABLE_CLUSTER_LABELS": ${ENABLE_CLUSTER_LABELS:-0},
-    "ENABLE_GCP_APIS": ${ENABLE_GCP_APIS:-0},
-    "ENABLE_GCP_IAM_ROLES": ${ENABLE_GCP_IAM_ROLES:-0},
-    "ENABLE_GCP_COMPONENTS": ${ENABLE_GCP_COMPONENTS:-0},
-    "ENABLE_REGISTRATION": ${ENABLE_REGISTRATION:-0},
-    "ENABLE_NAMESPACE_CREATION": ${ENABLE_NAMESPACE_CREATION:-0},
-    "DISABLE_CANONICAL_SERVICE": ${DISABLE_CANONICAL_SERVICE:-0},
-    "PRINT_CONFIG": ${PRINT_CONFIG:-0},
-    "SERVICE_ACCOUNT": "${SERVICE_ACCOUNT:-}",
-    "KEY_FILE": "${KEY_FILE:-}",
-    "OUTPUT_DIR": "${OUTPUT_DIR:-}",
-    "CA_CERT": "${CA_CERT:-}",
-    "CA_KEY": "${CA_KEY:-}",
-    "CA_ROOT": "${CA_ROOT:-}",
-    "CA_CHAIN": "${CA_CHAIN:-}",
-    "CA_NAME": "${CA_NAME:-}",
-    "DRY_RUN": ${DRY_RUN:-0},
-    "ONLY_VALIDATE": ${ONLY_VALIDATE:-0},
-    "ONLY_ENABLE": ${ONLY_ENABLE:-0},
-    "VERBOSE": ${VERBOSE:-0},
-    "MANAGED": ${MANAGED:-0},
-    "MANAGED_SERVICE_ACCOUNT": "${MANAGED_SERVICE_ACCOUNT:-}",
-    "PRINT_HELP": ${PRINT_HELP:-0},
-    "PRINT_VERSION": ${PRINT_VERSION:-0},
-    "CUSTOM_CA": ${CUSTOM_CA:-0},
-    "USE_HUB_WIP": ${USE_HUB_WIP:-0},
-    "USE_VM": ${USE_VM:-0},
-    "ENVIRON_PROJECT_ID": "${ENVIRON_PROJECT_ID:-}",
-    "HUB_MEMBERSHIP_ID": "${HUB_MEMBERSHIP_ID:-}",
-    "CUSTOM_REVISION": ${CUSTOM_REVISION:-0},
-    "WI_ENABLED": ${WI_ENABLED:-0}
-  },
-  "istioctlFiles": [],
-  "kubectlFiles": []
-}
-EOF
-)
-
 main() {
   init
 
+  context_init
   parse_args "${@}"
   validate_args
 
@@ -816,9 +764,6 @@ parse_args() {
   # shellcheck disable=SC2064
   trap "$(shopt -p nocasematch)" RETURN
   shopt -s nocasematch
-
-  # Init and export the global $context_FILE_LOCATION
-  context_init "${CONTEXT_JSON}"
 
   local OPTIONAL_OVERLAY; OPTIONAL_OVERLAY=""
   local CUSTOM_OVERLAY; CUSTOM_OVERLAY=""
--- asmcli/tests/lib/context.bats
+++ asmcli/tests/lib/context.bats
@@ -6,58 +6,7 @@ setup() {
   PROJECT_ID="this-is-a-test-project"
   CLUSTER_NAME="this-is-a-test-cluster"
   CLUSTER_LOCATION="us-east-2a"
-  CONTEXT_JSON=$(cat <<EOF
-{
-  "flags": {
-    "PROJECT_ID": "${PROJECT_ID:-}",
-    "CLUSTER_NAME": "${CLUSTER_NAME:-}",
-    "CLUSTER_LOCATION": "${CLUSTER_LOCATION:-}",
-    "MODE": "${MODE:-}",
-    "CA": "${CA:-}",
-    "KUBECONFIG": "${KUBECONFIG_FILE:-}",
-    "CONTEXT": "${CONTEXT:-}",
-    "CUSTOM_OVERLAY": "${CUSTOM_OVERLAY:-}",
-    "OPTIONAL_OVERLAY": "${OPTIONAL_OVERLAY:-}",
-    "ENABLE_ALL": ${ENABLE_ALL:-0},
-    "ENABLE_CLUSTER_ROLES": ${ENABLE_CLUSTER_ROLES:-0},
-    "ENABLE_CLUSTER_LABELS": ${ENABLE_CLUSTER_LABELS:-0},
-    "ENABLE_GCP_APIS": ${ENABLE_GCP_APIS:-0},
-    "ENABLE_GCP_IAM_ROLES": ${ENABLE_GCP_IAM_ROLES:-0},
-    "ENABLE_GCP_COMPONENTS": ${ENABLE_GCP_COMPONENTS:-0},
-    "ENABLE_REGISTRATION": ${ENABLE_REGISTRATION:-0},
-    "ENABLE_NAMESPACE_CREATION": ${ENABLE_NAMESPACE_CREATION:-0},
-    "DISABLE_CANONICAL_SERVICE": ${DISABLE_CANONICAL_SERVICE:-0},
-    "PRINT_CONFIG": ${PRINT_CONFIG:-0},
-    "SERVICE_ACCOUNT": "${SERVICE_ACCOUNT:-}",
-    "KEY_FILE": "${KEY_FILE:-}",
-    "OUTPUT_DIR": "${OUTPUT_DIR:-}",
-    "CA_CERT": "${CA_CERT:-}",
-    "CA_KEY": "${CA_KEY:-}",
-    "CA_ROOT": "${CA_ROOT:-}",
-    "CA_CHAIN": "${CA_CHAIN:-}",
-    "CA_NAME": "${CA_NAME:-}",
-    "DRY_RUN": ${DRY_RUN:-0},
-    "ONLY_VALIDATE": ${ONLY_VALIDATE:-0},
-    "ONLY_ENABLE": ${ONLY_ENABLE:-0},
-    "VERBOSE": ${VERBOSE:-0},
-    "MANAGED": ${MANAGED:-0},
-    "MANAGED_SERVICE_ACCOUNT": "${MANAGED_SERVICE_ACCOUNT:-}",
-    "PRINT_HELP": ${PRINT_HELP:-0},
-    "PRINT_VERSION": ${PRINT_VERSION:-0},
-    "CUSTOM_CA": ${CUSTOM_CA:-0},
-    "USE_HUB_WIP": ${USE_HUB_WIP:-0},
-    "USE_VM": ${USE_VM:-0},
-    "ENVIRON_PROJECT_ID": "${ENVIRON_PROJECT_ID:-}",
-    "HUB_MEMBERSHIP_ID": "${HUB_MEMBERSHIP_ID:-}",
-    "CUSTOM_REVISION": ${CUSTOM_REVISION:-0},
-    "WI_ENABLED": ${WI_ENABLED:-0}
-  },
-  "istioctlFiles": [],
-  "kubectlFiles": []
-}
-EOF
-)
-  context_init "${CONTEXT_JSON}"
+  context_init
 }
 
 # Potential cleanup work should happen here
