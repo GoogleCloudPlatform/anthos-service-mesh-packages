9e22934cb90ccb0336cce619232c76a58f0a0daf
Zhengzhe Yang <zhengzhey@google.com>
2021-05-13 17:31:42 -0400

    add validate/configure/interface to iterate on (#674)

---
+++ asmcli/commands/install.sh
@@ -0,0 +1,12 @@
+install_subcommand() {
+  # TODO: validate (mesh_ca | citadel | private_ca).
+  # TODO: validate (mcp | in-cluster) control plane.
+
+  # TODO: configure (mesh_ca | citadel | private_ca).
+  # TODO: configure (mcp | in-cluster) control plane.
+
+  # TODO: install (mesh_ca | citadel | private_ca).
+  # TODO: install (mcp | in-cluster) control plane.
+
+  return
+}
