67fb69f78197010c5d89fefcbe0d997dc74f739b
Apoorv Sharma <gzip@google.com>
2021-06-25 13:01:28 -0700

    --amend

--- asmcli/commands/install.sh
+++ asmcli/commands/install.sh
@@ -65,29 +65,6 @@ scrape_managed_urls() {
   echo "${VALIDATION_URL} ${CLOUDRUN_ADDR}"
 }
 
-# expects var PARAM already defined and populates it with the
-# appropriate manifest overlays
-init_install_params() {
-  local CA; CA="$(context_get-option "CA")"
-
-  PARAMS="-f ${OPERATOR_MANIFEST}"
-  for yaml_file in $(context_list "istioctlFiles"); do
-    PARAMS="${PARAMS} -f ${yaml_file}"
-  done
-
-  if [[ "${K8S_MINOR}" -eq 15 ]]; then
-    PARAMS="${PARAMS} -f ${BETA_CRD_MANIFEST}"
-  fi
-
-  if [[ "${CA}" == "citadel" ]]; then
-    PARAMS="${PARAMS} -f ${CITADEL_MANIFEST}"
-  fi
-
-  if ! is_gcp; then
-    PARAMS="${PARAMS} -f ${OFF_GCP_MANIFEST}"
-  fi
-}
-
 install_in_cluster_control_plane() {
   local USE_VM; USE_VM="$(context_get-option "USE_VM")"
 
