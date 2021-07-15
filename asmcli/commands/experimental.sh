d5845e4da0b958db690fadb412d0a7bd9513cbbc
Jason Wang <jasonwzm@google.com>
2021-07-15 14:08:07 -0700

    Add x vm prepare-cluster subcommand (#811)
    * add vm subcommand
    
    * stage changes
    
    * more changes
    
    * lint
    
    * address comments
    
    * rebase

--- asmcli/commands/experimental.sh
+++ asmcli/commands/experimental.sh
@@ -1,5 +1,14 @@
 experimental_subcommand() {
+  if [[ "${*}" = '' ]]; then
+    x_usage >&2
+    exit 2
+  fi
+
   case "${1}" in
+    vm)
+      shift 1
+      vm_subcommand "${@}"
+      ;;
     *)
       x_help_subcommand "${@}"
       ;;
