4af3d10d6724c139c18505068812513a6c0db717
Apoorv Sharma <gzip@google.com>
2021-08-04 14:07:40 -0700

    Use context cluster instead of name to detect GKE naming scheme

--- asmcli/lib/util.sh
+++ asmcli/lib/util.sh
@@ -569,8 +569,5 @@ EOF
 # Reads in stdin, expected in the form of kubectl config get-contexts
 # output and isolates the default context's cluster value.
 get_context_cluster() {
-  local CONTEXT_ROW
-  while IFS= read -r CONTEXT_ROW; do
-    echo "${CONTEXT_ROW}" | grep '^\*' | cut -c2- | sed 's/\([[:blank:]]\)*\1/\1/g' | cut -d ' ' -f 3
-  done
+  grep '^\*' | cut -c2- | sed 's/\([[:blank:]]\)*\1/\1/g' | cut -d ' ' -f 3
 }
