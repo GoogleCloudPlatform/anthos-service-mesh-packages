b8ee024504849dedd719aeb4f41c8d0ad30e5911
Apoorv Sharma <gzip@google.com>
2021-08-03 18:38:55 -0700

    Use context cluster instead of name to detect GKE naming scheme

--- asmcli/lib/validate.sh
+++ asmcli/lib/validate.sh
@@ -506,7 +506,6 @@ validate_args() {
   local WI_ENABLED; WI_ENABLED="$(context_get-option "WI_ENABLED")"
   local CONTEXT; CONTEXT="$(context_get-option "CONTEXT")"
   local KUBECONFIG_SUPPLIED; KUBECONFIG_SUPPLIED="$(context_get-option "KUBECONFIG_SUPPLIED")"
-  local KCF; KCF="$(context_get-option "KUBECONFIG")"
 
   if [[ -z "${CA}" ]]; then
     CA="mesh_ca"
@@ -583,7 +582,7 @@ EOF
   if [[ "${KUBECONFIG_SUPPLIED}" -eq 1 && -z "${CONTEXT}" ]]; then
     # set CONTEXT to current-context in the KUBECONFIG
     # or fail-fast if current-context doesn't exist
-    CONTEXT="$(kubectl --kubeconfig "${KCF}" config current-context)"
+    CONTEXT="$(kubectl config current-context)"
     if [[ -z "${CONTEXT}" ]]; then
       MISSING_ARGS=1
       warn "Missing current-context in the KUBECONFIG. Please provide context with --context flag or set a current-context in the KUBECONFIG"
@@ -595,7 +594,7 @@ EOF
   if [[ "${KUBECONFIG_SUPPLIED}" -eq 1 ]]; then
     info "Reading cluster information for ${CONTEXT}"
     local CONTEXT_CLUSTER;
-    CONTEXT_CLUSTER="$(kubectl --kubeconfig "${KCF}" config get-contexts --no-headers | cut -c2- | sed 's/\([[:blank:]]\)*\1/\1/g' | cut -d ' ' -f 3)"
+    CONTEXT_CLUSTER="$(kubectl config get-contexts --no-headers | cut -c2- | sed 's/\([[:blank:]]\)*\1/\1/g' | cut -d ' ' -f 3)"
     IFS="_" read -r _ PROJECT_ID CLUSTER_LOCATION CLUSTER_NAME <<EOF
 ${CONTEXT_CLUSTER}
 EOF
@@ -788,7 +787,7 @@ EOF
   if [[ "${KUBECONFIG_SUPPLIED}" -eq 1 ]]; then
     info "Reading cluster information for ${CONTEXT}"
     local CONTEXT_CLUSTER;
-    CONTEXT_CLUSTER="$(kubectl --kubeconfig "${KCF}" config get-contexts --no-headers | cut -c2- | sed 's/\([[:blank:]]\)*\1/\1/g' | cut -d ' ' -f 3)"
+    CONTEXT_CLUSTER="$(kubectl config get-contexts --no-headers | cut -c2- | sed 's/\([[:blank:]]\)*\1/\1/g' | cut -d ' ' -f 3)"
     IFS="_" read -r _ PROJECT_ID CLUSTER_LOCATION CLUSTER_NAME <<EOF
 ${CONTEXT_CLUSTER}
 EOF
