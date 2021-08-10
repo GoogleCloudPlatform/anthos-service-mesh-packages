ac092b211b9fb7d53839d716c06d1c722c1e54e9
Apoorv Sharma <gzip@google.com>
2021-08-10 14:00:26 -0700

    Apply topology.istio.io/network label to istio-system for MN topology

--- asmcli/commands/install.sh
+++ asmcli/commands/install.sh
@@ -189,10 +189,10 @@ install_control_plane() {
   local MANAGED; MANAGED="$(context_get-option "MANAGED")"
   local DISABLE_CANONICAL_SERVICE; DISABLE_CANONICAL_SERVICE="$(context_get-option "DISABLE_CANONICAL_SERVICE")"
 
+  label_istio_namespace
   if [[ "${MANAGED}" -eq 1 ]]; then
     install_managed_control_plane
   else
-    label_istio_namespace
     install_in_cluster_control_plane
   fi
 
