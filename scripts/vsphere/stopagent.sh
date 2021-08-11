e75ff540b37522d967b4594a4ca2e41f9f4de2f7
Linggg <linggg@google.com>
2021-08-10 23:35:49 -0700

    add deb VM support (#849)

---
+++ scripts/vsphere/stopagent.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+if [[ ! -z "${LOG_LEVEL}" ]]; then
+  set -ex
+else
+  set -CeE
+fi
+
+DIR="${1}"
+
+sudo systemctl stop istio
+
+if pidof envoy; then
+  kill $(eval pidof envoy)
+fi
+
+rm -rf /etc/certs/*
+rm -rf /var/log/istio
+rm -rf /var/run/secrets/tokens
+
+# remove obsolete istiod host on VM
+grep -v "istiod" "/etc/hosts" > "o"; mv "o" "/etc/hosts"
+
+cd ${DIR}
+rm -f cluster.env hosts istio-token mesh.yaml root-cert.pem runagent.sh
+rm -f istio-sidecar.rpm
+rm -f istio-sidecar.deb
+
+exit
