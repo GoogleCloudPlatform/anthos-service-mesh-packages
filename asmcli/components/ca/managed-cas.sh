7fe83cf2ce9446a28feb2d904144c6b4e1e378c9
lei-tang <32078630+lei-tang@users.noreply.github.com>
2022-06-07 15:58:17 -0700

    Configure managed CAS based on managed_cas.yaml (#1218)
    * Add the function to configure managed CAS based on managed_cas.yaml

---
+++ asmcli/components/ca/managed-cas.sh
@@ -0,0 +1,20 @@
+x_configure_managed_cas() {
+  local CUSTOM_OVERLAY; CUSTOM_OVERLAY="$(context_get-option "CUSTOM_OVERLAY")"
+
+  CUSTOM_OVERLAY="${OPTIONS_DIRECTORY}/managed_cas.yaml,${CUSTOM_OVERLAY}"
+  context_set-option "CUSTOM_OVERLAY" "${CUSTOM_OVERLAY}"
+
+  local CLUSTER_NAME; CLUSTER_NAME="$(context_get-option "CLUSTER_NAME")"
+  local CLUSTER_LOCATION; CLUSTER_LOCATION="$(context_get-option "CLUSTER_LOCATION")"
+  local USE_HUB_WIP; USE_HUB_WIP="$(context_get-option "USE_HUB_WIP")"
+  local HUB_MEMBERSHIP_ID; HUB_MEMBERSHIP_ID="$(context_get-option "HUB_MEMBERSHIP_ID")"
+  local HUB_IDP_URL; HUB_IDP_URL="$(context_get-option "HUB_IDP_URL")"
+
+  if [[ "${USE_HUB_WIP}" -eq 1 ]]; then
+    kpt cfg set asm anthos.servicemesh.idp-url "${HUB_IDP_URL}"
+  else
+    kpt cfg set asm anthos.servicemesh.idp-url "https://container.googleapis.com/v1/projects/${PROJECT_ID}/locations/${CLUSTER_LOCATION}/clusters/${CLUSTER_NAME}"
+  fi
+
+  configure_trust_domain_aliases
+}
