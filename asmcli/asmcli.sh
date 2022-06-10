0c84dc5cec2f915c9226df1b46443e110e0d653f
sergii-ssh <83605538+sergii-ssh@users.noreply.github.com>
2022-06-10 18:58:29 -0400

    Bump rev .4 (#1232)

--- asmcli/asmcli.sh
+++ asmcli/asmcli.sh
@@ -30,7 +30,7 @@ _CI_I_AM_A_TEST_ROBOT="${_CI_I_AM_A_TEST_ROBOT:=0}"; readonly _CI_I_AM_A_TEST_RO
 MAJOR="${MAJOR:=1}"; readonly MAJOR;
 MINOR="${MINOR:=11}"; readonly MINOR;
 POINT="${POINT:=8}"; readonly POINT;
-REV="${REV:=3}"; readonly REV;
+REV="${REV:=4}"; readonly REV;
 CONFIG_VER="${CONFIG_VER:="1-staging"}"; readonly CONFIG_VER;
 K8S_MINOR=0
 
