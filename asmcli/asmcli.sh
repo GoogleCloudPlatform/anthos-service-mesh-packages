d667abb3021ae96e5fa9201567ba1ee16a2bfa65
Clayton Pence <ctpence@google.com>
2023-01-06 23:11:21 +0000

    Bump config ver

--- asmcli/asmcli.sh
+++ asmcli/asmcli.sh
@@ -38,7 +38,7 @@ MAJOR="${MAJOR:=1}"; readonly MAJOR;
 MINOR="${MINOR:=13}"; readonly MINOR;
 POINT="${POINT:=9}"; readonly POINT;
 REV="${REV:=3}"; readonly REV;
-CONFIG_VER="${CONFIG_VER:="1"}"; readonly CONFIG_VER;
+CONFIG_VER="${CONFIG_VER:="2"}"; readonly CONFIG_VER;
 K8S_MINOR=0
 
 ### File related constants ###
