0158827e00b2a326e418365c85c3dc4ab44ae22d
Clayton Pence <ctpence@google.com>
2023-02-16 21:40:49 +0000

    Fix config ver

--- asmcli/asmcli.sh
+++ asmcli/asmcli.sh
@@ -38,7 +38,7 @@ MAJOR="${MAJOR:=1}"; readonly MAJOR;
 MINOR="${MINOR:=16}"; readonly MINOR;
 POINT="${POINT:=2}"; readonly POINT;
 REV="${REV:=2}"; readonly REV;
-CONFIG_VER="${CONFIG_VER:="1-unstable"}"; readonly CONFIG_VER;
+CONFIG_VER="${CONFIG_VER:="1-staging"}"; readonly CONFIG_VER;
 K8S_MINOR=0
 
 ### File related constants ###
