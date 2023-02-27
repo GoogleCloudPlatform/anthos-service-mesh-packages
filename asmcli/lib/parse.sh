3c8fcadef89480fc696f7ce9c153bd4618bff0b7
Clay <ctpence@google.com>
2023-02-27 11:49:40 -0800

    Remove reference to deprecated command (#1406)

--- asmcli/lib/parse.sh
+++ asmcli/lib/parse.sh
@@ -83,10 +83,6 @@ parse_args() {
 
         OPTIONAL_OVERLAY="${2},${OPTIONAL_OVERLAY}"
         context_set-option "OPTIONAL_OVERLAY" "${OPTIONAL_OVERLAY}"
-
-        if [[ "${2}" == "vm" ]]; then
-          context_set-option "USE_VM" 1
-        fi
         shift 2
         ;;
       --co | --custom_overlay | --custom-overlay)
