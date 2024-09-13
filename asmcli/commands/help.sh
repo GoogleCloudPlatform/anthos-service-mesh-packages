27cb8510c1da9ab7676a39544a254416558360f9
Sergii Shapar <sshapar@google.com>
2024-09-13 19:43:33 -0400

    wip

--- asmcli/commands/help.sh
+++ asmcli/commands/help.sh
@@ -182,11 +182,6 @@ FLAGS:
                                       package in the output directory. If the directory is not
                                       specified or does not contain the required files, the
                                       script will exit with error.
-     --print_config                   Instead of installing ASM, print all of
-                                      the compiled YAML to stdout. All other
-                                      output will be written to stderr, even if
-                                      it would normally go to stdout. Skip all
-                                      validations and setup.
      --disable_canonical_service      Do not install the CanonicalService
                                       controller. This is required for ASM UI to
                                       support various features.
@@ -278,7 +273,6 @@ FLAGS:
 
      --offline
 
-     --print_config
      --disable_canonical_service
   --managed_certificates
   -v|--verbose
