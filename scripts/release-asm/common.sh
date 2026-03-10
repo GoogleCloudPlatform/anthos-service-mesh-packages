0a1a4643d803db2a79e2a91044cf30878a09fd03
sriraju <krishnamsettys@google.com>
2026-03-10 04:02:46 +0000

    Bump Rev 1.29.0-asm.0

--- scripts/release-asm/common.sh
+++ scripts/release-asm/common.sh
@@ -24,9 +24,9 @@ trap 'gcloud storage objects update --no-temporary-hold gs://"${STABLE_VERSION_F
 
 prod_releases() {
   cat << EOF
-release 1.26
 release 1.27
 release 1.28
+release 1.29
 EOF
 }
 
@@ -34,9 +34,9 @@ CURRENT_RELEASE="$(prod_releases | tail -n 1)"; readonly CURRENT_RELEASE
 
 staging_releases() {
   cat << EOF
-staging 1.26
 staging 1.27
 staging 1.28
+staging 1.29
 EOF
 }
 
